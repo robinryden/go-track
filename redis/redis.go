c955a92c19868fd58ef33ac1c3f2cfbb925d1409
Robin <robin.ryden@strateg.se>
2018-02-27 16:48:57 +0100

    add: redis connection and env variables

--- redis/redis.go
+++ redis/redis.go
@@ -2,7 +2,20 @@ package redis
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/go-redis/redis"
 )
+
+func init() {
+	client := redis.NewClient(&redis.Options{
+		Addr:     os.Getenv("REDIS_HOST"),
+		Password: os.Getenv("REDIS_PASSWORD"),
+		DB:       0,
+	})
+
+	client.Set("Test key", "Test value", 0)
+}
 
 func TestFunction() {
 	fmt.Println("Redis database for logging")
