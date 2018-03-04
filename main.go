7c9b5e430446b3b8a8ce53583d61f9a3a9141460
Robin <robin.ryden@strateg.se>
2018-03-04 15:51:02 +0100

    Update lowercase packages and added cronjob

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
-	"go-track/Tracker"
+	"go-track/tracker"
 
+	"github.com/robfig/cron"
 	"github.com/subosito/gotenv"
 )
 
 var (
-	urls = []string{"http://www.strateg.se/testsomintefinns.html", "http://www.google.se", "http://www.blocket.se"}
+	urls = []string{"http://www.strateg.se/dajs.html", "http://www.google.se", "http://www.blocket.se"}
 )
 
 func init() {
@@ -16,14 +17,23 @@ func init() {
 }
 
 func main() {
-	go Tracker.Start()
+	go tracker.Start()
 
-	for _, url := range urls {
-		go func(url string) {
-			Tracker.TrackURL(url)
-		}(url)
-	}
+	cronWorker := cron.New()
+	cronWorker.AddFunc("1/5 * * * * *", func() {
+		go checkURLS()
+	})
+
+	cronWorker.Start()
 
 	var input string
 	fmt.Scanln(&input)
+}
+
+func checkURLS() {
+	for _, url := range urls {
+		go func(url string) {
+			tracker.TrackURL(url)
+		}(url)
+	}
 }
