7dde39d73d7db0df3567d2a04e300bd11ad58a6e
Robin <robin.ryden@strateg.se>
2018-03-04 15:52:15 +0100

    Update redis connection error handling

--- redis/redis.go
+++ redis/redis.go
@@ -16,17 +16,20 @@ type Log struct {
 	Time       time.Time
 }
 
-func Connect() *redis.Client {
+func Connect() (*redis.Client, error) {
 	client := redis.NewClient(&redis.Options{
 		Addr:     os.Getenv("REDIS_HOST"),
 		Password: os.Getenv("REDIS_PASSWORD"),
 		DB:       0,
 	})
-	return client
+	return client, nil
 }
 
 func Logger(url string, statusCode int, timestamp time.Time) {
-	client := Connect()
+	client, err := Connect()
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	logged := &Log{
 		url,
